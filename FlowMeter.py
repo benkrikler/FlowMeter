1999b27a8ca907089b6f8bcd67121554132e4be9
Ben Krikler <bek07@ic.ac.uk>
2015-03-01 14:37:45 +0000

    Fetch until a timedelta specified in the config

--- FlowMeter.py
+++ FlowMeter.py
@@ -5,6 +5,7 @@ import sys
 from modules import GetFlows
 
 default_config={
+	'date_offset':24 # hours to look for messages
         }
 config = configparser.ConfigParser(default_config)
 
@@ -16,5 +17,8 @@ if __name__ == "__main__":
     config.read(config_file)
 
     # Obtain the flow
-    flows=GetFlows.Connection(config.get("flowdock","token"))
-    flows.GetFlow("","comet","icedust-in-general")
+    flows=GetFlows.Connection(config.get("source","token"))
+    date_offset=config.getfloat("source","date_offset")
+    for org_flow in config.get("source","flows").split():
+	    [org,flow]=org_flow.split("/",1)
+	    flows.GetFlow(date_offset,org,flow)
--- modules/GetFlows.py
+++ modules/GetFlows.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import datetime
 
 REST_API_URL="https://api.flowdock.com/"
 
@@ -19,12 +20,30 @@ class Connection():
                 flow=flow,
                 service=service)
 
-    def GetFlow(self,date,organisation,flow):
+    def GetFlow(self,date_offset,organisation,flow):
         """ Get all the flows since date """
         # prepare the target URL
         url=self.MakeURL(organisation,flow,"messages")
-        r=requests.get(url, auth=(self.token,"")); 
-        objects=r.json()
-        for obj in objects:
-            #print(obj.keys())
-            print(obj["created_at"], obj["content"])
+
+        # What's the date we want to stop at?
+        stop_time=datetime.datetime.now() - datetime.timedelta(hours=date_offset)
+        print(stop_time)
+
+        # Pull messages until we've reached the requested time offset
+        reachedDesiredDate=False
+        last_id=-1
+        while(not reachedDesiredDate):
+            params={'limit':100}
+            if last_id != -1: params['until_id']=last_id
+            r=requests.get(url, auth=(self.token,""),params=params); 
+            objects=r.json()
+            for obj in reversed(objects):
+                #print(obj.keys())
+                created_at=datetime.datetime.strptime(obj["created_at"],"%Y-%m-%dT%H:%M:%S.%fZ")
+                last_id=int(obj["id"])
+                print(str(created_at),obj["created_at"], str(stop_time), created_at < stop_time)
+                if created_at < stop_time:
+                    reachedDesiredDate=True
+                    break
+                print(obj["user"], obj["content"])
+            #reachedDesiredDate=True
